cb870eaebcd288f0a8d796cad25a97d27dc1d1d3
João Almeida <jpcapoani98@gmail.com>
2020-08-08 17:53:06 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# VikingsFlix
+
+Esse projeto foi realizado durante a Imersão React da Alura
+
+O projeto está hospedado no link abaixo
+https://vikingsflix.vercel.app/
---
+++ src/pages/error/Erro404/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PageDefault from '../../../components/PageDefault';
+
+function Error404() {
+  return (
+    <PageDefault>
+      <h1>Erro 404 - Página não encontrada</h1>
+    </PageDefault>
+  );
+}
+
+export default Error404;
