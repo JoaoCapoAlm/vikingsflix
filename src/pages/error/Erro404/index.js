c05ac91d005c76620f68eabb0d3d2958eda9ca7c
João Pedro <62189134+JoaoCapoAlm@users.noreply.github.com>
2020-08-09 20:22:27 -0300

    Criação formulário de cadastro de vídeos e alteração de vídeos no db.json (#6)
    * Criação formulário de cadastro de vídeo
    
    * Alteraçõs vídeos db.json

--- src/pages/error/Erro404/index.js
+++ src/pages/error/Erro404/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Erro404.css';
 import Button from '../../../components/Button';
 import PageDefault from '../../../components/PageDefault';
 
