c93d4a9b709841132ef82759641fcfe83d3ba58d
João Almeida <jpcapoani98@gmail.com>
2020-08-10 21:57:54 -0300

    Formulário básico de cadastro de vídeos

--- src/repositories/categorias.js
+++ src/repositories/categorias.js
@@ -2,6 +2,18 @@ import config from '../config';
 
 const urlCategories = `${config.urlBackEnd}/categorias?_embed=videos`;
 
+function getAll() {
+  return fetch(`${urlCategories}`)
+    .then(async (respostaDoServidor) => {
+      if (respostaDoServidor.ok) {
+        const resposta = await respostaDoServidor.json();
+        return resposta;
+      }
+
+      throw new Error('Não possível pegar os dados!');
+    });
+}
+
 function getAllWithVideos() {
   return fetch(urlCategories)
     .then(async (respostaDoServidor) => {
@@ -16,4 +28,5 @@ function getAllWithVideos() {
 
 export default {
   getAllWithVideos,
+  getAll,
 };
