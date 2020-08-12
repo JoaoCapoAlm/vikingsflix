02cdfd84658e0797869cacef954566f7ca3b5b29
João Almeida <jpcapoani98@gmail.com>
2020-08-11 21:39:55 -0300

    Adicionado propTypes

--- src/components/Carousel/components/VideoCard/index.js
+++ src/components/Carousel/components/VideoCard/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { VideoCardContainer } from './styles';
 
 function getYouTubeId(youtubeURL) {
@@ -9,7 +10,6 @@ function getYouTubeId(youtubeURL) {
     );
 }
 
-
 function VideoCard({ videoTitle, videoURL, categoryColor }) {
   const image = `https://img.youtube.com/vi/${getYouTubeId(videoURL)}/hqdefault.jpg`;
   return (
@@ -17,10 +17,21 @@ function VideoCard({ videoTitle, videoURL, categoryColor }) {
       url={image}
       href={videoURL}
       target="_blank"
-      style={{ borderColor: categoryColor || 'red' }}
+      rel="noopener noreferrer"
+      style={{ borderColor: categoryColor }}
       title={videoTitle}
     />
   );
 }
+
+VideoCard.defaultProps = {
+  categoryColor: 'red',
+};
+
+VideoCard.propTypes = {
+  videoTitle: PropTypes.string.isRequired,
+  videoURL: PropTypes.string.isRequired,
+  categoryColor: PropTypes.string,
+};
 
 export default VideoCard;
