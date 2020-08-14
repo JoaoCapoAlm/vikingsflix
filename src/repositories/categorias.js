95978e132987a9023234de9251e7b095d42188cf
João Pedro <62189134+JoaoCapoAlm@users.noreply.github.com>
2020-08-14 19:06:19 -0300

    Criação da função para salvar novas categorias (#10)

--- src/repositories/categorias.js
+++ src/repositories/categorias.js
@@ -26,7 +26,25 @@ function getAllWithVideos() {
     });
 }
 
+function create(object) {
+  return fetch(urlCategories, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify(object),
+  })
+    .then(async (serverResponse) => {
+      if (serverResponse.ok) {
+        const answer = await serverResponse.json();
+        return answer;
+      }
+      throw new Error('Não foi possível cadastrar os dados');
+    });
+}
+
 export default {
   getAllWithVideos,
   getAll,
+  create,
 };
--- src/repositories/videos.js
+++ src/repositories/videos.js
@@ -2,18 +2,18 @@ import config from '../config';
 
 const urlVideos = `${config.urlBackEnd}/videos`;
 
-function create(objetoVideo) {
+function create(object) {
   return fetch(`${urlVideos}?_embed=videos`, {
     method: 'POST',
     headers: {
       'Content-type': 'application/json',
     },
-    body: JSON.stringify(objetoVideo),
+    body: JSON.stringify(object),
   })
-    .then(async (respostaDoServidor) => {
-      if (respostaDoServidor.ok) {
-        const resposta = await respostaDoServidor.json();
-        return resposta;
+    .then(async (serverResponse) => {
+      if (serverResponse.ok) {
+        const answer = await serverResponse.json();
+        return answer;
       }
 
       throw new Error('Não foi possível cadastrar os dados');
