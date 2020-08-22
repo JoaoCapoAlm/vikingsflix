4352d26132794bfe769c7d46881b8d9921ce32b2
João Almeida <jpcapoani98@gmail.com>
2020-08-22 12:21:02 -0300

    Movendo arquivo Menu.css

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
-import './Menu.css';
+import '../../styles/Menu.css';
 import Button from '../Button';
 
 function Menu() {
